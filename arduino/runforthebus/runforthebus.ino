ca1862a9ac2788fb620dce7484871fd3e1ff6d7e
jalonso <juan.alonso@tecnilogica.com>
2016-08-04 11:17:48 +0200

    working version

--- arduino/runforthebus/runforthebus.ino
+++ arduino/runforthebus/runforthebus.ino
@@ -1,7 +1,15 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-const int VOLTPIN = 3;
+const int VOLTPIN = 6;
+const int BUTTONPIN = 3;
+const int LEDPIN = 8;
+
+const int DATASAMPLES = 12;
+const long DELAYBETWEENSAMPLES = 10000;
+const int BLINK = 500;
+const int NOBUSVOLTS = 150;
+
 const long NOBUS = 999999;
 
 long waitingTime;
@@ -11,64 +19,112 @@ byte mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0xCC, 0x95};
 
 void setup() {
 
-  pinMode(VOLTPIN, OUTPUT);
+  pinMode(BUTTONPIN, INPUT_PULLUP);
 
+  pinMode(VOLTPIN, OUTPUT);
+  pinMode(LEDPIN, OUTPUT);
+  
   Serial.begin(9600);
-  while (!Serial){}; 
+  while (!Serial) {};
 
   if (! Ethernet.begin(mac)) {
     Serial.println("Could not connect to network");
-  } 
-
+  } else {
+    Serial.print("Local IP: ");
+    Serial.println(Ethernet.localIP());
+  }
   delay(1000);
-  
+
 }
 
 
 
 void loop() {
 
-    hitWebPage();
-    if (waitingTime != NOBUS) {
-      analogWrite (VOLTPIN, waitingTime);
-    } else {
-      analogWrite (VOLTPIN, 0);
+  if (digitalRead(BUTTONPIN) == LOW) {
+
+    digitalWrite(LEDPIN, HIGH);
+
+    Serial.println("Getting data");
+    for (int currentRep = 0; currentRep < DATASAMPLES; currentRep++) {
+
+      hitWebPage();
+
+      if (waitingTime != NOBUS) {
+        analogWrite (VOLTPIN, waitingTime);
+      } else {
+        analogWrite (VOLTPIN, NOBUSVOLTS);
+      }
+
+      if (waitingTime < 35) {
+
+        for (int f = 0; f < DELAYBETWEENSAMPLES / BLINK; f++) {
+          delay(BLINK/2);
+          digitalWrite(LEDPIN, LOW);
+          delay(BLINK/2);
+          digitalWrite(LEDPIN, HIGH);
+        }
+        
+      } else if (waitingTime < 70) {
+
+        for (int f = 0; f < DELAYBETWEENSAMPLES / (BLINK * 2); f++) {
+          delay(BLINK);
+          digitalWrite(LEDPIN, LOW);
+          delay(BLINK);
+          digitalWrite(LEDPIN, HIGH);
+        }
+        
+      } else {
+        
+        delay (DELAYBETWEENSAMPLES);
+        
+      }
+
     }
-    
-    for(int f=0; f<20; f++) {
-      Serial.print("*");
-      delay(500);
-    }
+
+    analogWrite (VOLTPIN, 0);
+    digitalWrite(LEDPIN, LOW);
+
+    Serial.println("End");
     Serial.println();
+
+  } else {
+
+    delay (100);
+
+  }
+
 
 }
 
 
 
+
 long hitWebPage() {
-  
+
   if (client.connect("neuronasdq.cluster005.ovh.net", 80)) {
-    
-    Serial.println("Getting data...");
-    
+
+    Serial.println("    Connecting");
+
     client.println("GET /bus/getbusinfo.php HTTP/1.0");
     client.println("Host: neuronasdq.cluster005.ovh.net");
     client.println();
-    
+
     while (client.connected()) {
       if (client.available()) {
         client.find("\"wait\":");
         waitingTime = client.parseInt();
+        Serial.print("    ");
         Serial.println(waitingTime);
         client.stop();
         return waitingTime;
       }
     }
-    
+
     client.stop();
 
   }
-  
+
   return NOBUS;
 
 }
